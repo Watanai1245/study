3b73c55d476dc218e003ae4e5eddd269121c44a0
Watanai1245 <69137758+Watanai1245@users.noreply.github.com>
2020-09-20 16:10:15 +0700

    Update triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
-int level;
-void moretriangle (int n){
-    for(int row=0;row<level;row++)
+struct triangle
+{
+    int level;
+}print;
+
+void moretriangle(int n) 
+{
+    struct triangle* p1 = &print;
+    for (int row = 0; row < p1->level; row++)
     {
-        for(int star=0;star<row+1;star++)
+        for (int star = 0; star < row + 1; star++)
         {
             printf("* ");
         }
@@ -11,21 +17,24 @@ void moretriangle (int n){
     }
 }
 
-void lesstriangle (int n){
-    for(int row = 0;row<=level;row++)
+void lesstriangle(int n) 
+{
+    struct triangle* p1 = &print;
+    for (int row = 0; row <= p1->level; row++)
     {
-       for(int star = level-(row-1);star>0;star--)
-       {
-           printf("* ");
-       }
+        for (int star = p1->level - (row - 1); star > 0; star--)
+        {
+            printf("* ");
+        }
         printf("\n");
     }
 }
 int main()
-{   
-    scanf("%d",&level);
-    moretriangle(level);
-    lesstriangle (level);
+{
+    struct triangle* p1 = &print;
+    scanf_s("%d", &p1->level);
+    moretriangle(p1->level);
+    lesstriangle(p1->level);
 
     return(0);
 }
